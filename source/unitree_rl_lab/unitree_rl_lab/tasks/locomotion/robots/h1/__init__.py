c5f7d866334960b541c0809c0dd4bdf2a0c5af69
AgnelWang <2273421791wk@gmail.com>
2025-07-14 16:53:39 +0800

    feature: parser play cfg by env_cfg_entry_point

--- source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/h1/__init__.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/h1/__init__.py
@@ -6,16 +6,7 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": f"{__name__}.velocity_env_cfg:RobotEnvCfg",
-        "rsl_rl_cfg_entry_point": f"unitree_rl_lab.tasks.locomotion.agents.rsl_rl_ppo_cfg:BasePPORunnerCfg",
-    },
-)
-
-gym.register(
-    id="Unitree-H1-Velocity-Play",
-    entry_point="isaaclab.envs:ManagerBasedRLEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": f"{__name__}.velocity_env_cfg:RobotPlayEnvCfg",
+        "play_env_cfg_entry_point": f"{__name__}.velocity_env_cfg:RobotPlayEnvCfg",
         "rsl_rl_cfg_entry_point": f"unitree_rl_lab.tasks.locomotion.agents.rsl_rl_ppo_cfg:BasePPORunnerCfg",
     },
 )
